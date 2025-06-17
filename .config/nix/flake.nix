0c129dd6f8ca91f82d2eb6aa4ef4871e6482e3d5
Dimitri Joy <dmtrjoy@gmail.com>
2025-06-17 00:42:21 -0400

    Enable Home Manager backups

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -7,39 +7,42 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-utils.url = "github:numtide/flake-utils";
+
     dotfiles.url = "github:dmtrjoy/dotfiles";
     dotfiles.flake = false;
   };
 
-  outputs = { self, nixpkgs, home-manager, dotfiles, ... }:
-    let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      homeConfigurations.dimitri = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          {
-            home.username = "dimitri";
-            home.homeDirectory = "/home/dimitri";
-            home.stateVersion = "24.05";
+  outputs = { self, nixpkgs, home-manager, flake-utils, dotfiles, ... }:
+    flake-utils.lib.eachDefaultSystemPassThrough (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        homeConfigurations.dimitri = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            {
+              home.username = "dimitri";
+              home.homeDirectory = "/home/dimitri";
+              home.stateVersion = "24.05";
+              home.backupFileExtension = ".backup";
 
-            home.file = {
-              ".bashrc".source = "${dotfiles}/.bashrc";
-              ".config/nvim/init.vim".source = "${dotfiles}/nvim/init.vim";
-              ".config/starship.toml".source = "${dotfiles}/starship.toml";
-            };
+              home.file = {
+                ".bashrc".source = "${dotfiles}/.bashrc";
+                ".config/nvim/init.vim".source = "${dotfiles}/nvim/init.vim";
+                ".config/starship.toml".source = "${dotfiles}/starship.toml";
+              };
 
-            home.packages = [
-              pkgs.luarocks
-              pkgs.ripgrep
-            ];
+              home.packages = [
+                pkgs.luarocks
+                pkgs.ripgrep
+              ];
 
-            programs.git.enable = true;
-            programs.neovim.enable = true;
-            programs.starship.enable = true;
-          }
-        ];
-      };
-    };
+              programs.git.enable = true;
+              programs.neovim.enable = true;
+              programs.starship.enable = true;
+            }
+          ];
+        };
+      });
 }
